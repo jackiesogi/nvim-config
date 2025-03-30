1bb06fe72b2bbea4d1b57bd97c7c1a2368c235ea
Jackie Chen <jackiesogi@gmail.com>
2025-03-31 03:33:07 +0800

    Fix 'dir already exists' issue

--- auto-install-linux.sh
+++ auto-install-linux.sh
@@ -1,5 +1,3 @@
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
 echo "This will overwrite your current nvim configuration, do you want to continue? [y/n]"
 
 read -r response
@@ -9,6 +7,12 @@ if [[ $response != "y" ]]; then
     exit 1
 fi
 
+rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+
+mkdir -p ~/.config/nvim
 cp -fvir * ~/.config/nvim/
 
 nvim -c 'PackerSync'
