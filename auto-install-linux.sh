372fe6c2ea75b64eca1b1c4f7bf40b6d5c7c580c
Shou-Chi Chen <jck.tux@proton.me>
2025-04-05 01:05:28 +0800

    Update auto installation script

--- auto-install-linux.sh
+++ auto-install-linux.sh
@@ -7,12 +7,20 @@ if [[ $response != "y" ]]; then
     exit 1
 fi
 
+sudo apt install nodejs lua5.1 ripgrep fdclone
+
 rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-
 mkdir -p ~/.config/nvim
+
 yes | cp -fvir * ~/.config/nvim/
 
+nvim +"so %" ~/.config/nvim/lua/config/plugins/packer.lua +q
+
 nvim -c 'PackerSync'
+
+git clone https://github.com/github/copilot.vim.git ~/.config/nvim/pack/github/start/copilot.vim
+
+nvim -c 'Copilot setup'
