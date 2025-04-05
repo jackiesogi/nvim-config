731f37b4f61172727dcf5edd1ca317d67cde9995
Jackie Chen <jackiesogi@gmail.com>
2025-04-06 07:01:16 +0800

    Update the auto installation script for installing nodejs version 20

--- auto-install-linux.sh
+++ auto-install-linux.sh
@@ -7,7 +7,14 @@ if [[ $response != "y" ]]; then
     exit 1
 fi
 
-sudo apt install nodejs lua5.1 ripgrep fdclone
+sudo apt install lua5.1 ripgrep fdclone
+
+sudo apt install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt update; sudo apt install -y nodejs
 
 rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
